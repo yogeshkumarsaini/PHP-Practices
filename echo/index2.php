d07906f8a2b49062b5907629e59b19b0bab725c1
Yogesh Kumar Saini <72929516+yogeshkumarsaini@users.noreply.github.com>
2025-09-30 22:53:08 +0530

    Add files via upload

---
+++ echo/index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h2>Hello World</h2>";
+    echo "Hello World!<br>";
+    echo "<p>Hello World</p>";
+    echo "This ","is ","PHP.";
+    ?>
+</body>
+</html>

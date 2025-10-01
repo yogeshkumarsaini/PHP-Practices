eb8f090b0c4987aa9d4f100d5ac37fb3dac49fcc
Yogesh Kumar Saini <72929516+yogeshkumarsaini@users.noreply.github.com>
2025-10-01 21:16:23 +0530

    Add files via upload

---
+++ echo/index3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $txt1 = "Learn PHP";
+    $txt2 = "Hello World!";
+
+    echo '<h2>' . $txt1 . '</h2>';
+    echo '<p>Study PHP at ' . $txt2 . '</p>';
+    ?>
+</body>
+</html>

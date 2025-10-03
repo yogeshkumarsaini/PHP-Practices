b5c6f95c255ab5b77abffc0e468d5fe152fd278c
Yogesh Kumar Saini <72929516+yogeshkumarsaini@users.noreply.github.com>
2025-10-03 23:17:04 +0530

    Add files via upload

---
+++ Data_Type/Null.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $x="Hello World!";
+    $x=null;
+    var_dump($x);
+    ?>
+</body>
+</html>
---
+++ Data_Type/object.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    class Car{
+        public $color;
+        public $model;
+        public function __construct($color,$model)
+        {
+            $this->color=$color;
+            $this->model=$model;
+        }
+        public function messgae(){
+            return "My Car is a".$this->color." ".$this->model." !";
+        }
+    }
+    $myCar=new Car("Red","Volvo");
+    var_dump($myCar);
+    ?>
+</body>
+</html>

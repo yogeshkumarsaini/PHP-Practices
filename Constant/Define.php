b73e055271c534749d369a6c2f6ce1cef76db967
Yogesh Kumar Saini <72929516+yogeshkumarsaini@users.noreply.github.com>
2025-10-07 22:06:02 +0530

    Add files via upload

---
+++ Constant/Define.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    define("GREETING","Welcome to Hello World");
+    echo GREETING;
+    ?>
+</body>
+</html>

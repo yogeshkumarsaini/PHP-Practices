c8141dc0591f4853c7d77ca8803d73e04ab6aac3
Yogesh Kumar Saini <72929516+yogeshkumarsaini@users.noreply.github.com>
2025-10-07 21:21:05 +0530

    Add files via upload

---
+++ Math/Min&Max.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo min(0,150,30,20,-8,-200);
+    echo "<br>";
+    echo max(0,150,30,30,-8,-200);
+    ?>
+</body>
+</html>
---
+++ Math/Round.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo round(0.60);
+    echo "<br>";
+    echo round(0.49);
+    ?>
+</body>
+</html>

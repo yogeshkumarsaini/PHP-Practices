0afe30d2fd36406943eb77b634461e4af2a147ce
Yogesh Kumar Saini <72929516+yogeshkumarsaini@users.noreply.github.com>
2025-11-19 22:25:18 +0530

    Add files via upload

---
+++ Form_Handling/from1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome_post.php" method="post">
+        Name: <input type="Text" name="name"><br>
+        Email: <input type="Text" name="email"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
